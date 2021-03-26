bc50533c63b064954017c34d914d7fb6812f1e98
F-Errami <errami2000@gmail.com>
2021-03-26 22:17:35 +0100

    .

--- include/ECS/Component.hpp
+++ include/ECS/Component.hpp
@@ -1,67 +1,66 @@
 #pragma once
-
+#ifndef SDL_ENGINE_COMPONENT_HPP
+#define SDL_ENGINE_COMPONENT_HPP
 #include "Entity.hpp"
 #include <unordered_map>
+#include <typeinfo>
+#include <memory>
 
-// The one instance of virtual inheritance in the entire implementation.
-// An interface is needed so that the ComponentManager (seen later)
-// can tell a generic ComponentArray that an entity has been destroyed
-// and that it needs to update its array mappings.
+
 class IComponentArray
 {
-public:
-    virtual ~IComponentArray() = default;
-    virtual void EntityDestroyed(Entity entity) = 0;
+    ~IComponentArray();
+    virtual void EntityDestroyed(Entity entity)
 };
 
 
-template<typename T>
-class ComponentArray : public IComponentArray
+template <typename T>
+class IComponentArray :public IComponentArray
 {
 public:
-    void InsertData(Entity entity, T component)
+void InsertData(Entity entity,T component)
+{
+    assert(mEntityToIndexMap.find(entity) == mEntityToIndexMap.end() && "Component added to same entity more than once.");
+    //we insert the component at the last of the array
+    size_t newIndex = size;
+    EntityToIndexMap[entity] = newIndex;
+    IndexToEntity[newIndex] = entity;
+    componentArray[newIndex] = component;
+    ++size;
+}
+
+void removeData(Entity entity)
+{
+    assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "Removing non-existent component.");
+
+    //we put the component of the entity at the last of the array
+    size_t indexOfRemovedEntity = EntityToIndexMap[entity];
+    size_t indexOfLastEntity = size-1;
+    componentArray[indexOfRemovedEntity] = componentArray[indexOfLastEntity];
+
+// we just change the index of the last entity with the index of the removed one
+    Entity lastEntity = IndexToEntityMap[indexOfLastEntity];
+    IndexToEntityMap[indexOfRemovedEntity] = lastEntity;
+    EntityToIndexMap[lastEntity]=indexOfRemovedEntity;
+//we delete the last entity
+    EntityToIndexMap.erase(entity);
+    IndexToEntityMap.erase(indexOfLastEntity);
+
+    --size;
+
+
+}
+
+T& getData(Entity entity)
+{
+    assert(EntityToIndexMap.find(entity) != EntityToIndexMap.end() && "Retrieving non-existent component.");
+
+    return componentArray[EntityToIndexMap[entity]];
+}
+
+void EntityDestroyed(Entity entity) override
     {
-        assert(mEntityToIndexMap.find(entity) == mEntityToIndexMap.end() && "Component added to same entity more than once.");
-
-        // Put new entry at end and update the maps
-        size_t newIndex = mSize;
-        mEntityToIndexMap[entity] = newIndex;
-        mIndexToEntityMap[newIndex] = entity;
-        mComponentArray[newIndex] = component;
-        ++mSize;
-    }
-
-    void RemoveData(Entity entity)
-    {
-        assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "Removing non-existent component.");
-
-        // Copy element at end into deleted element's place to maintain density
-        size_t indexOfRemovedEntity = mEntityToIndexMap[entity];
-        size_t indexOfLastElement = mSize - 1;
-        mComponentArray[indexOfRemovedEntity] = mComponentArray[indexOfLastElement];
-
-        // Update map to point to moved spot
-        Entity entityOfLastElement = mIndexToEntityMap[indexOfLastElement];
-        mEntityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
-        mIndexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
-
-        mEntityToIndexMap.erase(entity);
-        mIndexToEntityMap.erase(indexOfLastElement);
-
-        --mSize;
-    }
-
-    T& GetData(Entity entity)
-    {
-        assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "Retrieving non-existent component.");
-
-        // Return a reference to the entity's component
-        return mComponentArray[mEntityToIndexMap[entity]];
-    }
-
-    void EntityDestroyed(Entity entity) override
-    {
-        if (mEntityToIndexMap.find(entity) != mEntityToIndexMap.end())
+        if (EntityToIndexMap.find(entity) != EntityToIndexMap.end())
         {
             // Remove the entity's component if it existed
             RemoveData(entity);
@@ -69,98 +68,39 @@ public:
     }
 
 private:
-    // The packed array of components (of generic type T),
-    // set to a specified maximum amount, matching the maximum number
-    // of entities allowed to exist simultaneously, so that each entity
-    // has a unique spot.
-    std::array<T, MAX_ENTITIES> mComponentArray;
+    //an array of components with the time
+    //each entity has her own component
+    std::array <T,MAX_ENTITY> componentArray;
 
-    // Map from an entity ID to an array index.
-    std::unordered_map<Entity, size_t> mEntityToIndexMap;
 
-    // Map from an array index to an entity ID.
-    std::unordered_map<size_t, Entity> mIndexToEntityMap;
+//it allows us to know the index of an entity in the componentArray
+//so that we can know the data of an entity by just knowing her id
+    std::unordere_map<Entity,size_t> EntityToIndexMap;
 
-    // Total size of valid entries in the array.
-    size_t mSize;
+//it allows us to know the Entity of a component in the componentArray
+//so that we can know an entity  by just knowing the index of her component
+    std::unordered_map <size_t, Entity> IndexToEntityMap;
+
+
+//total of valid entries in the array
+    size_t size;
+
+
 };
-
 
 class ComponentManager
 {
 public:
-    template<typename T>
-    void RegisterComponent()
-    {
-        const char* typeName = typeid(T).name();
 
-        assert(mComponentTypes.find(typeName) == mComponentTypes.end() && "Registering component type more than once.");
-
-        // Add this component type to the component type map
-         mComponentTypes.insert({typeName, mNextComponentType});
-
-        // Create a ComponentArray pointer and add it to the component arrays map
-       mComponentArrays.insert({typeName, std::make_shared<ComponentArray<T> >()});
-
-        // Increment the value so that the next component registered will be different
-        ++mNextComponentType;
-    }
-
-    template<typename T>
-    ComponentType GetComponentType()
-    {
-        const char* typeName = typeid(T).name();
-
-        assert(mComponentTypes.find(typeName) != mComponentTypes.end() && "Component not registered before use.");
-
-        // Return this component's type - used for creating signatures
-        return mComponentTypes[typeName];
-    }
-
-    template<typename T>
-    void AddComponent(Entity entity, T component)
-    {
-        // Add a component to the array for an entity
-        GetComponentArray<T>()->InsertData(entity, component);
-    }
-
-    template<typename T>
-    void RemoveComponent(Entity entity)
-    {
-        // Remove a component from the array for an entity
-        GetComponentArray<T>()->RemoveData(entity);
-    }
-
-    template<typename T>
-    T& GetComponent(Entity entity)
-    {
-        // Get a reference to a component from the array for an entity
-        return GetComponentArray<T>()->GetData(entity);
-    }
-
-    void EntityDestroyed(Entity entity)
-    {
-        // Notify each component array that an entity has been destroyed
-        // If it has a component for that entity, it will remove it
-        for (auto const& pair : mComponentArrays)
-        {
-            auto const& component = pair.second;
-
-            component->EntityDestroyed(entity);
-        }
-    }
 
 private:
-    // Map from type string pointer to a component type
-    std::unordered_map<const char*, ComponentType> mComponentTypes;
+    std::unordered_map<const char*,ComponentType> componentTypes;
 
-    // Map from type string pointer to a component array
-    std::unordered_map<const char*, std::shared_ptr<IComponentArray> > mComponentArrays;
+    std::unordered_map<const char*,std::shared_ptr<IComponentArray> componentArrayss;
 
-    // The component type to be assigned to the next registered component - starting at 0
-    ComponentType mNextComponentType;
+    ComponentType NextComponentType{};
 
-    // Convenience function to get the statically casted pointer to the ComponentArray of type T.
+
     template<typename T>
     std::shared_ptr<ComponentArray<T> > GetComponentArray()
     {
@@ -168,6 +108,9 @@ private:
 
         assert(mComponentTypes.find(typeName) != mComponentTypes.end() && "Component not registered before use.");
 
-        return std::static_pointer_cast<ComponentArray<T> >(mComponentArrays[typeName]);
+        return std::static_pointer_cast<ComponentArray<T>>(mComponentArrays[typeName]);
     }
 };
+};
+
+#endif //SDL_ENGINE_COMPONENT_HPP
--- include/ECS/Entity.hpp
+++ include/ECS/Entity.hpp
@@ -2,58 +2,34 @@
 #ifndef SDL_ENGINE_ENTITY_HPP
 #define SDL_ENGINE_ENTITY_HPP
 #include <iostream>
-#include <memory>
 #include <bitset>
-#include <algorithm>
+#include <queue>
 #include <array>
-#include <vector>
-#include <assert.h>
-#include <unordered_map>
-#include <memory>
-#include<queue>
 
-
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
-
-
-// A simple type alias
-using Entity = std::uint32_t;
-
-// Used to define the size of arrays later on
-const Entity MAX_ENTITIES = 5000;
-
-
-// A simple type alias
-using ComponentType = std::uint8_t;
-
-// Used to define the size of arrays later on
-const ComponentType MAX_COMPONENTS = 32;
-// A simple type alias
-using Signature = std::bitset<MAX_COMPONENTS>;
+//an Entity is just an ID
+using Entity = uint32_t;
+const Entity MAX_ENTITY = 5000;
+using ComponentType = size_t;
+cont ComponentType MAX_COMPONENT=32;
+using Signature = std::bitset <MAX_COMPONENT>;
 
 class EntityManager
 {
 public:
     EntityManager()
     {
-        // Initialize the queue with all possible entity IDs
-        for (Entity entity = 0; entity < MAX_ENTITIES; ++entity)
+        for (Entity entity = 0; entity < MAX_ENTITY; ++i)
         {
-            mAvailableEntities.push(entity);
+            availableEntities.push(entity);
         }
+        livingEntity = 0;
     }
 
-    Entity CreateEntity()
-    {
-        assert(mLivingEntityCount < MAX_ENTITIES && "Too many entities in existence.");
-
-        // Take an ID from the front of the queue
-        Entity id = mAvailableEntities.front();
-        mAvailableEntities.pop();
-        ++mLivingEntityCount;
+    Entity createEntity()
+    {  assert(nbLivingEntity < MAX_ENTITIES && "Too many entities in existence.");
+        id = availableEntities.front();
+        availableEntities.pop();
+        ++nbLivingEntity;
 
         return id;
     }
@@ -61,40 +37,34 @@ public:
     void DestroyEntity(Entity entity)
     {
         assert(entity < MAX_ENTITIES && "Entity out of range.");
-
-        // Invalidate the destroyed entity's signature
-        mSignatures[entity].reset();
-
-        // Put the destroyed ID at the back of the queue
-        mAvailableEntities.push(entity);
-        --mLivingEntityCount;
+        availableEntities.push(entity);
+        signatures[entity].resest();
+        --nbLivingEntity;
     }
 
-    void SetSignature(Entity entity, Signature signature)
+    void setSignatures( Entity entity, Signature signature)
     {
         assert(entity < MAX_ENTITIES && "Entity out of range.");
-
-        // Put this entity's signature into the array
-        mSignatures[entity] = signature;
+        signatures[entity] = signature;
     }
 
-    Signature GetSignature(Entity entity)
+   Signature getSigmature(Entity entity)
     {
         assert(entity < MAX_ENTITIES && "Entity out of range.");
-
-        // Get this entity's signature from the array
-        return mSignatures[entity];
+        return signatures[entity];
     }
+
 
 private:
-    // Queue of unused entity IDs
-    std::queue<Entity> mAvailableEntities;
+    //entity that are available
+    std::queue<Entity> availableEntities;
+    //an array of the signature of each entity where the index is the entity
+    std::array<Signature,MAX_ENTITY> signatures;
+    //the number of entity that are created
+    uint32_t nbLivingEntity;
 
-    // Array of signatures where the index corresponds to the entity ID
-    std::array<Signature, MAX_ENTITIES> mSignatures;
 
-    // Total living entities - used to keep limits on how many exist
-    uint32_t mLivingEntityCount;
 };
+
 
 #endif //SDL_ENGINE_ENTITY_HPP
