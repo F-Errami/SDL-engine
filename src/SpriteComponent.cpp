b28ba37d496618b77c5ab4d19fd546e387a80be9
F-Errami <errami2000@gmail.com>
2021-04-11 17:59:04 +0000

    .

--- src/SpriteComponent.cpp
+++ src/SpriteComponent.cpp
@@ -3,7 +3,9 @@
 SpriteComponent::SpriteComponent()
 {}
 SpriteComponent::~SpriteComponent()
-{}
+{
+    SDL_DestroyTexture(texture);
+}
 SpriteComponent::SpriteComponent(const char* fileName)
 {
     texture = TextureManager::LoadTexture(fileName);
