36ebafacc72af4e4ce1f81b3c0a471893f423261
F-Errami <errami2000@gmail.com>
2021-04-12 13:22:03 +0000

    .

--- include/ECS/ColliderComponent.hpp
+++ include/ECS/ColliderComponent.hpp
@@ -7,9 +7,26 @@ class ColliderComponent
 {
     SDL_Rect collider;
     std::string tag;
-    TransformComponent* transform;
+    TransformComponent& transform;
 
-    void init
+    ColliderComponent()
+    {}
+    init(TransformComponent& transform)
+    {
+        this->transfor m= transform;
+    }
+
+    void update()
+    {
+        colloder.x = transform.position.x;
+        collider.y = transform.position.y;
+        collider.w = transform.width * transform.scale;
+        collider.h = transform.height * transform.scale;
+    }
+
+
+
+
 
 };
 #endif //SDL_ENGINE_COLLIDERCOMPONENT_HPP
