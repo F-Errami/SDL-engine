8f483a5bc34d85ba4ba769d779592b5ff04a8c79
F-Errami <errami2000@gmail.com>
2021-04-14 18:49:48 +0000

    .

---
+++ src/ColliderComponent.cpp
@@ -0,0 +1,14 @@
+#include "../include/ECS/ColliderComponent.hpp"
+
+void ColliderComponent:: init(TransformComponent& transform)
+{
+    this->transform= transform;
+}
+
+void ColliderComponent ::update()
+{
+    collider.x = transform.position.x;
+    collider.y = transform.position.y;
+    collider.w = transform.width * transform.scale;
+    collider.h = transform.height * transform.scale;
+}
